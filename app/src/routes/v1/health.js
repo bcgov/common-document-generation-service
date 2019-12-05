2846a09adb323f814c0fa7f177a42bd5d0061d28
Lucas O'Neil <lucasoneil@gmail.com>
2019-12-04 16:35:10 -0800

    Fix health

--- app/src/routes/v1/health.js
+++ app/src/routes/v1/health.js
@@ -3,7 +3,7 @@ const healthRouter = require('express').Router();
 const checkComponent = require('../../components/health');
 
 /** Returns the status of correspondent APIs */
-healthRouter.get('/status', async (_req, res, next) => {
+healthRouter.get('/', async (_req, res, next) => {
   const statuses = await checkComponent.getStatus();
 
   if (statuses instanceof Array) {
