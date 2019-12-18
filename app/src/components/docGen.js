0df61a93486ca65bae98cece9321d243b1d3321b
Jeremy Ho <jujaga@gmail.com>
2019-12-17 16:53:41 -0800

    Fix temp files not being deleted Add in work in progress working Dockerfile Simplify temp file extension handling
    Signed-off-by: Jeremy Ho <jujaga@gmail.com>

--- app/src/components/docGen.js
+++ app/src/components/docGen.js
@@ -1,42 +1,37 @@
-const log = require('npmlog');
 const carbone = require('carbone');
+const fs = require('fs');
+const log = require('npmlog');
 const stream = require('stream');
 const tmp = require('tmp');
-const fs = require('fs');
 const utils = require('./utils');
+
+tmp.setGracefulCleanup();
 
 const docGen = {
   /** Generate a file using carbone to merge data into the supplied document template
    *  @param {object} body The request body
    *  @param {object} response The server response to write the generated file to
    */
-  generateDocument: async (body, response) => {
-    tmp.setGracefulCleanup();
-    let tmpFile = undefined;
+  generateDocument: (body, response) => {
+    let tmpFile;
     try {
-      tmpFile = tmp.fileSync();
+      const inboundFileExtension = utils.getFileExtension(body.template.filename);
+
+      tmpFile = tmp.fileSync({ postfix: `.${inboundFileExtension}` });
       if (!body.template.contentEncodingType) {
         body.template.contentEncodingType = 'base64';
       }
 
-      // Put the actual extension from the supplied file onto the tmp filename
-      const inboundFileExtension = utils.getFileExtension(body.template.filename);
-      const tmpFilename = inboundFileExtension ? `${tmpFile.name}.${inboundFileExtension}` : tmpFile.name;
-      log.debug('generateDocument', 'Filename: ' + tmpFilename);
+      log.debug('generateDocument', 'Filename: ' + tmpFile.name);
 
-      await fs.promises.writeFile(tmpFilename, Buffer.from(body.template.content, body.template.contentEncodingType));
+      fs.writeFileSync(tmpFile.name, Buffer.from(body.template.content, body.template.contentEncodingType));
       log.debug('generateDocument', JSON.stringify(tmpFile));
 
       // Set options
       const options = {
+        convertTo: inboundFileExtension,
         reportName: body.template.filename
       };
-
-      // Only supporting pdf conversion at the moment
-      if (inboundFileExtension && inboundFileExtension.toUpperCase() === 'PDF') {
-        log.debug('setting pdf generation option');
-        options.convertTo = 'pdf';
-      }
 
       // If it's not an array of multiple data items, pass it into carbone as a singular object
       const data = body.contexts.length > 1 ? body.contexts : body.contexts[0];
@@ -44,7 +39,7 @@ const docGen = {
       // TODO: there's too much response stuff down here in a component layer, figure out how
       // better to have the asynchronous carbone render be blocking and wait for its response
       // up in the v1/docGen.js route layer, then handle response setting there.
-      carbone.render(tmpFilename, data, options, (err, result, reportName) => {
+      carbone.render(tmpFile.name, data, options, (err, result, reportName) => {
         if (err) {
           const errTxt = `Error during Carbone generation. Error: ${err}`;
           log.error('generateDocument', errTxt);
