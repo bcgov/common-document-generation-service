07c4ee030e66501f32889f8f40e5859f54297ab9
Jason Sherman <jsherman@parcsystems.ca>
2020-02-12 14:00:47 -0800

    Revert "Revert "add and rename configuration.""
    This reverts commit cdebd1a13f643cf6cd72444b9cb474ace14302ff.

--- app/tests/unit/common-logging/common-logging-queue.spec.js
+++ app/tests/unit/common-logging/common-logging-queue.spec.js
@@ -1,6 +1,7 @@
 const helper = require('../../common/helper');
 
 const CommonLoggingQueue = require('../../../src/common-logging/common-logging-queue');
+const Constants = require('../../../src/common-logging/common-logging-constants');
 
 helper.logHelper();
 
@@ -8,26 +9,26 @@ describe('common-logging-queue constructor', () => {
 
   test('constructor, uses defaults when no options', () => {
     const clogsQueue = new CommonLoggingQueue();
-    expect(clogsQueue._batchSize).toBe(50);
-    expect(clogsQueue._batchTimeout).toBe(Infinity);
-    expect(clogsQueue._initialDelay).toBe(1000);
+    expect(clogsQueue._maxBatchSize).toBe(Constants.QUEUE_DEFAULT_MAX_BATCH_SIZE);
+    expect(clogsQueue._batchTimeout).toBe(Constants.QUEUE_DEFAULT_BATCH_TIMEOUT);
+    expect(clogsQueue._initialDelay).toBe(Constants.QUEUE_DEFAULT_INITIAL_DELAY);
   });
 
   test('constructor, accepts options', () => {
-    const clogsQueue = new CommonLoggingQueue({batchSize: 1, batchTimeout: 5000, initialDelay: 10000});
-    expect(clogsQueue._batchSize).toBe(1);
+    const clogsQueue = new CommonLoggingQueue({maxBatchSize: 1, batchTimeout: 5000, initialDelay: 10000});
+    expect(clogsQueue._maxBatchSize).toBe(1);
     expect(clogsQueue._batchTimeout).toBe(5000);
     expect(clogsQueue._initialDelay).toBe(10000);
   });
 
   test('constructor, defaults bad options', () => {
     const clogsQueue = new CommonLoggingQueue({
-      batchSize: 1,
+      maxBatchSize: 1,
       batchTimeout: {value: 'five thousand'},
       initialDelay: 10000
     });
-    expect(clogsQueue._batchSize).toBe(1);
-    expect(clogsQueue._batchTimeout).toBe(Infinity);
+    expect(clogsQueue._maxBatchSize).toBe(1);
+    expect(clogsQueue._batchTimeout).toBe(Constants.QUEUE_DEFAULT_BATCH_TIMEOUT);
     expect(clogsQueue._initialDelay).toBe(10000);
   });
 
@@ -36,13 +37,13 @@ describe('common-logging-queue constructor', () => {
 describe('common-logging-queue push/flush', () => {
 
   test('push adds to queue', async () => {
-    const clogsQueue = new CommonLoggingQueue({batchSize: 1, initialDelay: Infinity});
+    const clogsQueue = new CommonLoggingQueue({maxBatchSize: 1, initialDelay: Infinity});
     await clogsQueue.push({message: 'hi'});
     expect(clogsQueue._queue.getLength()).toBe(1);
   });
 
   test('flush gets batch from queue', async () => {
-    const clogsQueue = new CommonLoggingQueue({batchSize: 1, initialDelay: Infinity});
+    const clogsQueue = new CommonLoggingQueue({maxBatchSize: 1, initialDelay: Infinity});
     await clogsQueue.push({message: 'hi'});
     await clogsQueue.push({message: 'there'});
     expect(clogsQueue._queue.getLength()).toBe(2);
@@ -52,7 +53,7 @@ describe('common-logging-queue push/flush', () => {
   });
 
   test('flush (all) gets all from queue', async () => {
-    const clogsQueue = new CommonLoggingQueue({batchSize: 1, initialDelay: Infinity});
+    const clogsQueue = new CommonLoggingQueue({maxBatchSize: 1, initialDelay: Infinity});
     await clogsQueue.push({message: 'hi'});
     await clogsQueue.push({message: 'there'});
     expect(clogsQueue._queue.getLength()).toBe(2);
@@ -70,7 +71,7 @@ describe('common-logging-queue flush timer', () => {
   });
 
   test('flush called on timeout', async () => {
-    const clogsQueue = new CommonLoggingQueue({batchSize: 1, batchTimeout: 10000, initialDelay: 10000});
+    const clogsQueue = new CommonLoggingQueue({maxBatchSize: 1, batchTimeout: 10000, initialDelay: 10000});
     const spy = jest.spyOn(clogsQueue, 'flush');
     await clogsQueue.push({message: 'hi'});
     await jest.runAllTimers();
