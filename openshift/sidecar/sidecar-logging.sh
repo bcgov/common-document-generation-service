721bb8eafe1c1d5789a289394b476538a727676f
Jason Sherman <jsherman@parcsystems.ca>
2020-02-21 17:08:08 -0800

    no message

--- openshift/sidecar/sidecar-logging.sh
+++ openshift/sidecar/sidecar-logging.sh
@@ -3,10 +3,6 @@
 # debug on fail
 set -euo pipefail
 
-if [ "x${DEBUG}" = xtrue ]; then
-  set -x
-fi
-
 # required
 : "${DEDUPE:?You must set deduplication}"
 : "${POD_NAME:?Pod name should be set from downward api}"
